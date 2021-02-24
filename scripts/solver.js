15d9d9bd4514737139abc6801df3c872f3703065
Christian Duvick <nr7256bm@go.minnstate.edu>
2021-02-24 11:28:44 -0600

    Started validator method

--- scripts/solver.js
+++ scripts/solver.js
@@ -1,16 +1,30 @@
-/*
-    The game board is represented as a 2D Array
-    here is an example
+const testBoard = [
+    [1,2,3,4,5,6,7,8,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+]
 
-        [1, 0, 0, 0, 0, 0, 0, 0, 0]
-        [0, 2, 0, 0, 0, 0, 0, 0, 0]
-        [0, 0, 3, 0, 0, 0, 0, 0, 0]
-        [0, 0, 0, 4, 0, 0, 0, 0, 0]
-        [0, 0, 0, 0, 5, 0, 0, 0, 0]
-        [0, 0, 0, 0, 0, 6, 0, 0, 0]
-        [0, 0, 0, 0, 0, 0, 7, 0, 0]
-        [0, 0, 0, 0, 0, 0, 0, 8, 0]
+/*
+    This method gets the input values from the DOM and turns it into a array.
+    The game board is represented as a 2D Array
+    here is an example:
+    [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 3, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 4, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 5, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 6, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 7, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 8, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 9]
+    ]
  */
 function getGameBoard() {
     const inputs = document.querySelectorAll("input");
@@ -29,3 +43,25 @@ function getGameBoard() {
     }
     return board;
 }
+
+/*
+    This method will be the validator. It will check if a number is valid at any given position on the board.
+ */
+function isValid (gameBoard, value, positionArray) {
+    const boardSize = gameBoard.length;
+
+    //Checking if its valid given the row.
+    //need to debug
+    for (let index = 0; index < boardSize; index++) {
+        console.log(`[${[positionArray[0]]}][${index}] = ` + (gameBoard[positionArray[0][index]]));
+        if ((gameBoard[positionArray[0][index]] === value) && (index !== positionArray[1])) {
+            return false;
+        }
+    }
+
+
+
+    return true;
+}
+let array = [0,8]
+isValid(testBoard, 8, array);
